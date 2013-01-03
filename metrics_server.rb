efa0c97460603e76bea5278aae60af368a1c012f
Matthaus Owens <matthaus@puppetlabs.com>
2013-01-02 23:08:44 -0800

    Add error handling to the routes for lack of db info.

--- metrics_server.rb
+++ metrics_server.rb
@@ -16,52 +16,72 @@ $: << "#{File.dirname(__FILE__)}/models"
 end
 
 class MetricServer < Sinatra::Base
-  config_file = "#{File.dirname(__FILE__)}/conf/db.conf"
-
-  # Bail if no config
-  unless File.exists?(config_file)
-    STDERR.puts "wtf. no config file at #{config_file}"
-    exit 1
+  attr_accessor :metrics, :avg, :error
+  def self.config_file
+    ["/etc/metrics/db.conf", "#{File.dirname(__FILE__)}/conf/db.conf"].each do |config|
+      return config if File.exists?(config)
+    end
+    nil
   end
-  config = YAML.load_file(config_file)
 
-  # If you want the logs displayed you have to do this before the call to setup
-  DataMapper::Logger.new($stdout, :debug)
-  config_string = "postgres://#{config['username']}:#{config['password']}@#{config['hostname']}#{config.has_key?('port') ? ":#{config['port']}" : ""}/#{config['database']}"
-  # A Postgres connection:
-  DataMapper.setup(:default, config_string)
-  require "metric"
+  @config = YAML.load_file(config_file) if config_file
 
-  attr_accessor :metrics, :avg
+  if @config
+    # If you want the logs displayed you have to do this before the call to setup
+    DataMapper::Logger.new($stdout, :debug)
+    config_string = "postgres://#{@config['username']}:#{@config['password']}@#{@config['hostname']}#{@config.has_key?('port') ? ":#{@config['port']}" : ""}/#{@config['database']}"
+    # A Postgres connection:
+    DataMapper.setup(:default, config_string)
+    require "metric"
+  end
 
   get '/' do
-    @metrics = Metric.all
-    slim :home
-  end
-
-  get '/package/:package' do
-    @metrics = Metric.all(:package => params[:package])
-    sum = 0
-    @metrics.each { |row| sum += row.build_time }
-    @avg = sum.to_f / @metrics.size.to_f
-    slim :package
-  end
-
-  post '/metrics' do
-    begin
-      # Do math on minutes section, combine with seconds bits.
-      if (time = params[:build_time].match(/(\d*)m(\d*\.\d*)s/))
-        params[:build_time] = (time[1].to_i * 60).to_f + time[2].to_f
-      # Strip the trailing s from `time` submissions.
-      elsif (time = params[:build_time].match(/(\d*\.\d*)s/))
-        params[:build_time] = time[1]
-      end
-      Metric.create( params )
-      [200, "Sweet"]
-    rescue Exception => e
-      [418, "#{e.message} AND #{params.inspect}"]
+    if @config
+      @metrics = Metric.all
+      slim :home
+    else
+      @error = "No db configuration information could be found in /etc/metrics/db.conf or /usr/share/metrics/conf/db.conf"
+      slim :error
     end
   end
 
-  run! if app_file == $0
+  get '/package/:package' do
+    if @config
+      @metrics = Metric.all(:package => params[:package])
+      sum = 0
+      @metrics.each { |row| sum += row.build_time }
+      @avg = sum.to_f / @metrics.size.to_f
+      slim :package
+    else
+      @error = "No db configuration information could be found in /etc/metrics/db.conf or /usr/share/metrics/conf/db.conf"
+      slim :error
+    end
+  end
+
+  post '/metrics' do
+    if @config
+      begin
+        # Do math on minutes section, combine with seconds bits.
+        if (time = params[:build_time].match(/(\d*)m(\d*\.\d*)s/))
+          params[:build_time] = (time[1].to_i * 60).to_f + time[2].to_f
+        # Strip the trailing s from `time` submissions.
+        elsif (time = params[:build_time].match(/(\d*\.\d*)s/))
+          params[:build_time] = time[1]
+        end
+        Metric.create( params )
+        [200, "Sweet"]
+      rescue Exception => e
+        [418, "#{e.message} AND #{params.inspect}"]
+      end
+    else
+      slim :error
+      @error = "No db configuration information could be found in /etc/metrics/db.conf or /usr/share/metrics/conf/db.conf"
+    end
+  end
+
+  error do
+    slim :error
+  end
+
+  run!
 end
